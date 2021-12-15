990f35449651a551c556d4ab5cc27593fd68ee45
Ali TEK <alitek@gmail.com>
2021-12-15 23:53:13 +0300

    0

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ var favicon = require('serve-favicon')
 var methodOverride = require('method-override')
 //var indexRouter = require('./routes/index')
 var dbLoader = require('./db/db-loader')
-var httpServer=require('./lib/http-server.js')
+var httpServer = require('./lib/http-server.js')
 
-global.staticValues=loadJSONFile(path.join(__root,'/resources/static-values.json'))
-global.portalModules=loadJSONFile(path.join(__root,'/resources/portal-modules.json'))
-global.version='20210916'
+global.staticValues = loadJSONFile(path.join(__root, '/resources/static-values.json'))
+global.portalModules = loadJSONFile(path.join(__root, '/resources/portal-modules.json'))
+global.version = '20210916'
 
 
 global.app = express()
@@ -20,45 +20,44 @@ var cors = require('cors')
 app.use(cors())
 var flash = require('connect-flash')
 
-app.use(favicon(path.join(__dirname,'resources','web-icon.png')))
+app.use(favicon(path.join(__dirname, 'resources', 'web-icon.png')))
 
 app.use(logger('dev'))
-app.use(bodyParser.json({limit: "100mb"}))
-app.use(bodyParser.urlencoded({limit: "100mb", extended: true, parameterLimit:50000}))
+app.use(bodyParser.json({ limit: "100mb" }))
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, parameterLimit: 50000 }))
 app.use(cookieParser())
 app.use(methodOverride())
 
 //indexRouter(app)
 
 
-app.set('port',config.httpserver.port)
+app.set('port', config.httpserver.port)
 
 global.fileImporter = require('./lib/file_importer')
 global.documentHelper = require('./lib/document_helper')
 global.printHelper = require('./lib/print_helper')
-global.programs=require('./services/programs/programs')
-global.auth=require('./lib/rest-helper')(config.passport_api)
+global.programs = require('./services/programs/programs')
+global.auth = require('./lib/rest-helper')(config.passport_api)
 
-process.on('uncaughtException', function (err) {
-	errorLog('Caught exception: ', err)
-	
-	if(config.status!='development'){
-		mail.sendErrorMail(`${(new Date()).yyyymmddhhmmss()} ${app.get('name')} Error`,errObj)
-		
-	}
-})
+if(config.status != 'development') {
+	process.on('uncaughtException', function(err) {
+		errorLog('Caught exception: ', err)
+		mail.sendErrorMail(`${(new Date()).yyyymmddhhmmss()} ${app.get('name')} Error`, err)
+	})
+}
 
 
-module.exports=()=>{
-	httpServer(app,(err,server,port)=>{
-		dbLoader((err)=>{
-			if(!err){
+
+module.exports = () => {
+	httpServer(app, (err, server, port) => {
+		dbLoader((err) => {
+			if(!err) {
 				require('./init-static-values.js')()
 				require('./routes/index')(app)
 				testControllers(false)
 				// console.log(`restServices:`,restServices)
 				// console.log(`config.restServices:`,config.restServices)
-			}else{
+			} else {
 				errorLog(err)
 			}
 		})
@@ -67,35 +66,34 @@ module.exports=()=>{
 
 
 /* [CONTROLLER TEST] */
-function testControllers(log){
-	moduleLoader(path.join(__dirname,'controllers'),'.controller.js',(log?'master controllers testing':''),(err,holder)=>{
-		if(!err){
+function testControllers(log) {
+	moduleLoader(path.join(__dirname, 'controllers'), '.controller.js', (log ? 'master controllers testing' : ''), (err, holder) => {
+		if(!err) {
 			eventLog(`test master controllers OK ${Object.keys(holder).length.toString().yellow}`)
-			moduleLoader(path.join(__dirname,'controllers/system'),'.controller.js',(log?'system controllers testing':''),(err,holder)=>{
-				if(!err){
+			moduleLoader(path.join(__dirname, 'controllers/system'), '.controller.js', (log ? 'system controllers testing' : ''), (err, holder) => {
+				if(!err) {
 					eventLog(`test system controllers OK ${Object.keys(holder).length.toString().yellow}`)
-					moduleLoader(path.join(__dirname,'controllers/web'),'.controller.js',(log?'web controllers testing':''),(err,holder)=>{
-						if(!err){
+					moduleLoader(path.join(__dirname, 'controllers/web'), '.controller.js', (log ? 'web controllers testing' : ''), (err, holder) => {
+						if(!err) {
 							eventLog(`test web controllers OK ${Object.keys(holder).length.toString().yellow}`)
-							moduleLoader(path.join(__dirname,'controllers/repo'),'.controller.js',(log?'repo controllers testing':''),(err,holder)=>{
-								if(!err){
+							moduleLoader(path.join(__dirname, 'controllers/repo'), '.controller.js', (log ? 'repo controllers testing' : ''), (err, holder) => {
+								if(!err) {
 									eventLog(`test repository controllers OK ${Object.keys(holder).length.toString().yellow}`)
-								}else{
+								} else {
 									throw err
 								}
 							})
-						}else{
+						} else {
 							throw err
 						}
 					})
-					
-				}else{
+
+				} else {
 					throw err
 				}
 			})
-		}else{
+		} else {
 			throw err
 		}
 	})
 }
-
